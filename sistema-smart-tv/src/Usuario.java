add36ca22b4557075d5e3d89dc5f20cbadaf6ba3
Félix David <feliztunda3@gmail.com>
2024-05-23 12:45:18 -0300

    Conta Banco

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+
+
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada ?" + smartTv.ligada);
+        System.out.println("Canal Atual :" + smartTv.canal);
+        System.out.println("Volume Atual :" + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> Tv Ligada ? " + smartTv.ligada);
+
+
+    }
+}
